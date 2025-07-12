fed65e09d0e5127f09f6e4311de792c9264ca4f3
GK <gabykassab0@gmail.com>
2025-07-12 14:54:01 +0300

    first commit

--- config/db.js
+++ config/db.js
@@ -36,17 +36,36 @@
 
 
 
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+//   ssl: {
+//     rejectUnauthorized: false // ✅ This is required for Render PostgreSQL
+//   }
+// });
+
+// module.exports = pool;
+
+
+
+
+
+
+
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // ✅ This is required for Render PostgreSQL
+    rejectUnauthorized: false
   }
 });
 
