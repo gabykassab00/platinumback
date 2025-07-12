ab0af2ae3234c45ef51a23da137fcc86b56227e7
GK <gabykassab0@gmail.com>
2025-07-12 16:55:55 +0300

    first commit

--- app.js
+++ app.js
@@ -24,8 +24,36 @@
 
 
 
+// const express = require('express');
+// const cors = require('cors');
+// const productRoutes = require('./routes/productRoutes');
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use('/api/products', productRoutes);
+
+// // Static files (for images)
+// app.use('/images', express.static('C:/Users/Computop/Desktop/perfumes/back/backend/images'));
+
+// module.exports = app;
+
+
+
+
+
+
+
+
+
+
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const productRoutes = require('./routes/productRoutes');
 
 const app = express();
@@ -34,10 +62,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+// API Routes
 app.use('/api/products', productRoutes);
 
-// Static files (for images)
-app.use('/images', express.static('C:/Users/Computop/Desktop/perfumes/back/backend/images'));
+// Serve images from the /images folder (relative to this file)
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
